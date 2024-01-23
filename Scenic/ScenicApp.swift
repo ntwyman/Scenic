f28df4c3f369e6d62927b893a22cdbc9d763ffdf
Nick Twyman <nick@conky.org>
2024-01-22 17:18:03 -0800

    Initial Commit

---
+++ Scenic/ScenicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScenicApp.swift
+//  Scenic
+//
+//  Created by Nick Twyman on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ScenicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
